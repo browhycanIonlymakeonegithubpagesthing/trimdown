ce7e73fd15ce2be22e9862a64070bb5d678c1bc9
insert-text-here <125519936+bro-my-username-got-blocked@users.noreply.github.com>
2023-10-23 19:40:39 -0700

    testestste

---
+++ static/public/test/js/uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/test/google/',
+    bare: '/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/test/js/uv/uv.handler.js',
+    bundle: '/test/js/uv/uv.bundle.js',
+    config: '/test/js/uv/uv.config.js',
+    sw: '/test/js/uv/uv.sw.js',
+};
