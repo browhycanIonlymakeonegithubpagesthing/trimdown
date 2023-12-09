3388f9e3df0eff30e1d48383e0651db2f2061787
insert-name-here .github.io <474652@bsd48.org>
2023-12-09 09:45:15 +0000

    more stuff

--- static/public/script.js
+++ static/public/script.js
@@ -55,62 +55,69 @@ function GameLoader() {
     let iframe = document.getElementById('contentFrame');
     iframe.srcdoc = `
     <style>
-        .hidden { display: none; }
-        body {
-            font-family: Arial, sans-serif;
-        }
-        
-        #searchBox {
-            width: 100%;
-            padding: 10px;
-            margin: 10px 0;
-            font-size: 16px;
-        }
-        
-        button.loadIframe {
-            display: block;
-            width: 100%;
-            padding: 10px;
-            margin: 10px 0;
-            background-color: #4CAF50;
-            color: white;
-            border: none;
-            text-align: left;
-            cursor: pointer;
-        }
-        
-        button.loadIframe:hover {
-            background-color: #45a049;
-        }
-        
-        iframe {
-            border: none;
-            height: 100vh;
-            width: 100%;
-        }
-        
-    </style>
+    .hidden { display: none; }
+    body {
+    font-family: Arial, sans-serif;
+}
+
+#searchBox {
+    width: 100%;
+    padding: 10px;
+    margin: 10px 0;
+    font-size: 16px;
+}
+
+button.loadIframe {
+    display: block;
+    width: 100%;
+    padding: 10px;
+    margin: 10px 0;
+    background-color: #4CAF50;
+    color: white;
+    border: none;
+    text-align: left;
+    cursor: pointer;
+}
+
+button.loadIframe:hover {
+    background-color: #45a049;
+}
+
+iframe {
+    border: none;
+}
+
+</style>
 </head>
 <body>
-    <input type="text" id="searchBox" placeholder="Search for a site...">
-    <button class="loadIframe" data-url="./Games/rb">Retro Bowl</button>
-    <button class="loadIframe" data-url="./Games/pk">Progress Knight</button>
-    <button class="loadIframe" data-url="./Games/te">Tetris</button>
-    <button class="loadIframe" data-url="./Games/sb">SnowBattle</button>
-    <button class="loadIframe" data-url="./Games/ngn">Ngon New</button>
-    <button class="loadIframe" data-url="./Games/ngo">Ngon OG</button>
-    <button class="loadIframe" data-url="./Games/whg">World's Hardest Game (real)</button>
-    <button class="loadIframe" data-url="./Games/whg2">World's Hardest Game 2 (real)</button>
-    <button class="loadIframe" data-url="./Games/ib">Idle Breakout</button>
-    <button class="loadIframe" data-url="./Games/cc">Cookie Clicker (version 2.048)</button>
-    <button class="loadIframe" data-url="./Games/br">The Backrooms</button>
-    <script>
+<input type="text" id="searchBox" placeholder="Search for a site...">
+<button class="loadIframe" data-url="./Games/rb">Retro Bowl</button>
+<button class="loadIframe" data-url="./Games/pk">Progress Knight</button>
+<button class="loadIframe" data-url="./Games/te">Tetris</button>
+<button class="loadIframe" data-url="./Games/sb">SnowBattle</button>
+<button class="loadIframe" data-url="./Games/ngn">Ngon New</button>
+<button class="loadIframe" data-url="./Games/ngo">Ngon OG</button>
+<button class="loadIframe" data-url="./Games/whg">World's Hardest Game (real)</button>
+<button class="loadIframe" data-url="./Games/whg2">World's Hardest Game 2 (real)</button>
+<button class="loadIframe" data-url="./Games/ib">Idle Breakout</button>
+<button class="loadIframe" data-url="./Games/cc">Cookie Clicker (version 2.048)</button>
+<button class="loadIframe" data-url="./Games/br">The Backrooms</button>
+<button class="loadIframe" data-url="./Games/1v1">1v1.LOL</button>
+<button class="loadIframe" data-url="./Games/ss">Subway Surfers</button>
+<button class="loadIframe" data-url="./Games/ts">Tanuki Sunset</button>
+<button class="loadIframe" data-url="./Games/fs">Funny Shooter</button>
+<button class="loadIframe" data-url="https://turbowarp.org/292728003/embed">Two Player Battle</button>
+<button class="loadIframe" data-url="https://turbowarp.org/389464290/embed">Getting Over It</button>
+<button class="loadIframe" data-url="https://turbowarp.org/105500895/embed">Geometry Dash (levels 1-3)</button>
+<button class="loadIframe" data-url="https://turbowarp.org/216343253/embed">Geometry Dash Subzero</button>
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
         var buttons = document.querySelectorAll('.loadIframe');
 
         buttons.forEach(function(button) {
             button.addEventListener('click', function() {
                 var url = this.getAttribute('data-url');
-                document.body.innerHTML = '<iframe src="' + url + '"></iframe>';
+                document.body.innerHTML = '<iframe src="' + url + '" style="width:100%; height:100vh;"></iframe>';
             });
         });
 
@@ -118,13 +125,14 @@ function GameLoader() {
             var searchText = e.target.value.toLowerCase();
             buttons.forEach(function(button) {
                 if (button.textContent.toLowerCase().includes(searchText)) {
-                    button.classList.remove('hidden');
+                    button.style.display = 'block';
                 } else {
-                    button.classList.add('hidden');
+                    button.style.display = 'none';
                 }
             });
         });
-    </script>
+    });
+</script>
  `;
 }
 function hideIframeContent() {
