337571bd948ddde53592324c069b423e67654720
insert-name-here .github.io <474652@bsd48.org>
2023-12-22 22:43:40 +0000

    work in progress

--- static/public/script.js
+++ static/public/script.js
@@ -4,6 +4,7 @@ function HideButtons() {
     document.getElementById('button3').style.display='none';
     document.getElementById('button4').style.display='none';
     document.getElementById('button5').style.display='none';
+    document.getElementById('alarmClock').style.display='none';
     document.getElementById('backButton').style.display = 'block';
 }
 function GetButtons() {
@@ -12,6 +13,7 @@ function GetButtons() {
     document.getElementById('button3').style.display='block';
     document.getElementById('button4').style.display='block';
     document.getElementById('button5').style.display='block';
+    document.getElementById('alarmClock').style.display='block';
     document.getElementById('backButton').style.display = 'none'; 
 }
 function ProxyFullscreen() {
@@ -75,4 +77,18 @@ function hideIframeContent() {
     GetButtons();
     let iframe = document.getElementById('contentFrame');
     iframe.srcdoc = '';
+}
+function openNewWindow() {
+    const selectedOption = document.getElementById("Periods").value;
+    const scheduleData = JSON.parse(localStorage.getItem(selectedOption));
+
+    // Assuming the link is in the first row of the schedule table
+    if (scheduleData && scheduleData.length > 0) {
+        const urlFromSchedule = scheduleData[0].link;
+        if (urlFromSchedule.startsWith("https://")) {
+            window.open(urlFromSchedule, '_blank');
+        } else {
+            window.open(`https://${urlFromSchedule}`, '_blank');
+        }
+    }
 }
